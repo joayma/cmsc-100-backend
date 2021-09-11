7d3187576a62bef27ff3283c38f97b03cfbe58c3
joayma <jamustapha@up.edu.ph>
2021-09-12 06:47:19 +0800

    test: add testing for app.js

---
+++ test/app.test.js
@@ -0,0 +1,26 @@
+const { build } = require('../app');
+require('tap').mochaGlobals();
+require('should');
+
+describe('For the route for root (/)', () => {
+    let app;
+    before(async () => {
+        // initialize backend application
+        app = await build({
+            logger: false
+        });
+    });
+    it('it should return { success: true } when called using GET',  async () => {
+        const response = await app.inject({
+            method: 'GET',
+            url: '/'
+        });
+
+        const payload = response.json();
+        const { statusCode } = response;
+        const { success } = payload;
+
+        success.should.equal(true);
+        statusCode.should.equal(200);
+    });
+});
