59e895ee6f2e10d7231d5a8d8b698245c974f34f
joayma <jamustapha@up.edu.ph>
2021-09-14 06:28:59 +0800

    feat: add connection setup for mongoose

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// connect to the mongodb instance
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/**
+ * 
+ * Creates a connection to the dabatase
+ * @returns {Promise}
+ */
+exports.connect = () => new Promise((resolve, reject) => {
+    const connection = mongoose;
+    connection.on('error', reject);
+    connection.once('open', resolve);
+})
+
+exports.mongoose = mongoose;
